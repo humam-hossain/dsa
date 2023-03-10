748af90bb438682672399223897e0cd09e94a7e4
humam-hossain <humam.hossain.e@gmail.com>
2023-03-10 23:53:05 +0600

    submited

--- algorithomic_toolbox/src/fibonacci.cpp
+++ algorithomic_toolbox/src/fibonacci.cpp
@@ -50,8 +50,8 @@ int main() {
     int n = 0;
     std::cin >> n;
 
-    std::cout << fibonacci_naive(n) << '\n';
-    test_solution();
+    // std::cout << fibonacci_naive(n) << '\n';
+    // test_solution();
     std::cout << fibonacci_fast(n) << '\n';
     return 0;
 }
