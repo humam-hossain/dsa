fc53cbc30f3fe5e2f6ff6df640fd1ef05f03d4fa
humam-hossain <humam.hossain.e@gmail.com>
2023-03-31 02:05:42 +0600

    setup

---
+++ algorithomic_toolbox/src/binary_puzzle.py
@@ -0,0 +1,20 @@
+low = int(input())
+high = int(input())
+
+mid = low + (high - low)//2
+print(mid)
+
+while True:
+    case = input()
+    
+    if case == '<':
+        # print("less")
+        high = mid - 1
+    elif case == '>':
+        low = mid + 1
+        # print("high")
+    else:
+        print("result: " + str(mid))
+        break
+    mid = low + (high - low)//2
+    print(mid)
